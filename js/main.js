6f24d4d4f9eeb0bb92337824df8a9b11ce2768f1
klaus <infogatmus@gmail.com>
2026-08-29 04:04:03 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 
+const languageButton = document.querySelector(".language-toggle");
+
+const languageContainer = document.querySelector(".language-container");
+
+
+languageButton.addEventListener("click", function () {
+
+    languageContainer.classList.toggle("active");
+
+});
+
+
+
+const languageButtons = document.querySelectorAll(".language-menu button");
+
+
+languageButtons.forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        languageContainer.classList.remove("active");
+
+    });
+
+});
